5796dd26f56ff641abd66999d328a0781ca7d13a
mori-dev <mori.dev.asdf@gmail.com>
2014-01-05 22:29:28 +0900

    Add Japanese option

--- tool/get_assets.sh
+++ tool/get_assets.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+if [ ${NODE_LANG} = "ja" ]; then
+  docs_url="http://nodejs.jp/nodejs.org_ja/docs"
+else
+  docs_url="http://nodejs.org/docs"
+fi
+
 if [ ! -e public/docs/assets ]; then
   mkdir -p public/docs/assets
 fi
 
 for asset in style.css sh.css
 do
-  echo "Download http://nodejs.org/docs/${NODE_VERSION}/api/assets/${asset}"
-  curl http://nodejs.org/docs/${NODE_VERSION}/api/assets/${asset} > public/docs/assets/${asset}
+  echo "Download ${docs_url}/${NODE_VERSION}/api/assets/${asset}"
+  curl ${docs_url}/${NODE_VERSION}/api/assets/${asset} > public/docs/assets/${asset}
 done
 
 for asset in sh_main.js sh_javascript.min.js
 do
-  echo "Download http://nodejs.org/docs/${NODE_VERSION}/${asset}"
-  curl http://nodejs.org/docs/${NODE_VERSION}/${asset} > public/docs/assets/${asset}
+  echo "Download ${docs_url}/${NODE_VERSION}/${asset}"
+  curl ${docs_url}/${NODE_VERSION}/${asset} > public/docs/assets/${asset}
 done
